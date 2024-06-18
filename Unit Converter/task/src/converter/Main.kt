2c1c9bdb2701b62488d2f8cd75c0e60976e2b865
james <james1.6@hotmail.com>
2024-06-18 18:20:52 +0930

    Stage 2
    Implement dynamic unit conversion
    
    The hardcoded unit conversion messages have been replaced with a dynamic conversion system. It now takes user input for a number and a measure, then parses and converts the input. Questions about improperly formatted input or unrecognized units are now dealt with in this system as well.

--- Unit Converter/task/src/converter/Main.kt
+++ Unit Converter/task/src/converter/Main.kt
@@ -1,13 +1,57 @@
 package converter
 
 fun main() {
-    println(
-        """
-            145 centimeters is 1.45 meters
-            2 miles is 3.2187 kilometers
-            5.5 inches is 139.7 millimeters
-            12 degrees Celsius is 53.6 degrees Fahrenheit
-            3 pounds is 1.360776 kilograms
-        """.trimIndent()
-    )
+    print("Enter a number and a measure: ")
+    val userInput = readln()
+    println()
+    val parsedInput: Pair<Double, Units>? = parseInput(userInput)
+    if (parsedInput == null) {
+        println("Wrong input")
+        return
+    }
+    val amount = parsedInput.first
+    val fromUnitName = parsedInput.second.name.lowercase().pluralise(amount)
+    val result: Double = convert(parsedInput)
+    val toUnitName = "meter".pluralise(result)
+    println("${amount.dropTrail()} $fromUnitName is ${result.dropTrail()} $toUnitName")
 }
+
+private fun Double.dropTrail(): String {
+    return (if (this % 1 == 0.0) this.toInt() else this).toString()
+}
+
+private fun String.pluralise(d: Double): String {
+    return if (d == 1.0) {
+        this
+    } else this + "s"
+}
+
+fun convert(pair: Pair<Double, Units>): Double {
+    return pair.first * pair.second.SIFactor
+}
+
+fun parseInput(userInput: String): Pair<Double, Units>? {
+    val (i, unit) = userInput.uppercase().split(" ")
+    try {
+        i.toInt()
+    } catch (_: NumberFormatException) {
+        return null
+    }
+    return when {
+        unit == "KM" -> Pair(i.toDouble(), Units.KILOMETER)
+        i.toInt() > 1 && unit == "KILOMETERS" -> Pair(i.toDouble(), Units.KILOMETER)
+        i.toInt() == 1 && unit == "KILOMETER" -> Pair(i.toDouble(), Units.KILOMETER)
+        else -> null
+    }
+
+}
+
+
+
+
+enum class Units(val SIFactor: Double) {
+    KILOMETER(1000.0),
+    M(1.0),
+    CM(0.01);
+}
+
