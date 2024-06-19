4e2b3da91109f14dc5ec4cb57c25c595f5d2d58b
james <james1.6@hotmail.com>
2024-06-19 15:29:15 +0930

    Stage 3 - 4
    Refactor Unit Converter for continuous conversions and error handling
    
    The Unit Converter has been significantly refactored to improve functionality and user experience. It now supports continuous conversions until the user decides to exit and enhances error handling for invalid input. Simplified logic also enhance the readability and maintainability of the code.

--- Unit Converter/task/src/converter/Main.kt
+++ Unit Converter/task/src/converter/Main.kt
@@ -1,57 +1,99 @@
 package converter
 
 fun main() {
-    print("Enter a number and a measure: ")
-    val userInput = readln()
-    println()
-    val parsedInput: Pair<Double, Units>? = parseInput(userInput)
-    if (parsedInput == null) {
-        println("Wrong input")
-        return
+    while (true) {
+        print("Enter what you want to convert (or exit): ")
+        val userInput = readln()
+        if (userInput == "exit") {
+            return
+        }
+        println()
+        val request: ConversionRequest
+        try {
+            request = parseInput(userInput)
+        } catch (e: Exception) {
+            println(e.message)
+            continue
+        }
+        val amount = request.i
+        val fromUnitName = request.fromName
+        val result: Double = convert(request)
+        val toUnitName = request.to.getPluralisedName(result)
+        println("$amount $fromUnitName is $result $toUnitName")
+        println()
     }
-    val amount = parsedInput.first
-    val fromUnitName = parsedInput.second.name.lowercase().pluralise(amount)
-    val result: Double = convert(parsedInput)
-    val toUnitName = "meter".pluralise(result)
-    println("${amount.dropTrail()} $fromUnitName is ${result.dropTrail()} $toUnitName")
+
 }
 
-private fun Double.dropTrail(): String {
-    return (if (this % 1 == 0.0) this.toInt() else this).toString()
+fun convert(request: ConversionRequest): Double {
+    val baseUnitAmount = request.i * request.from.factor
+    return baseUnitAmount / request.to.factor
 }
 
-private fun String.pluralise(d: Double): String {
-    return if (d == 1.0) {
-        this
-    } else this + "s"
-}
-
-fun convert(pair: Pair<Double, Units>): Double {
-    return pair.first * pair.second.SIFactor
-}
-
-fun parseInput(userInput: String): Pair<Double, Units>? {
-    val (i, unit) = userInput.uppercase().split(" ")
+fun parseInput(userInput: String): ConversionRequest {
+    val (iString, fromString, toString) = userInput.lowercase().split(" ").filterIndexed { i, v -> i != 2 }
+    val i: Double
     try {
-        i.toInt()
+        i = iString.toDouble()
     } catch (_: NumberFormatException) {
-        return null
+        throw NumberFormatException("Wrong input.")
     }
-    return when {
-        unit == "KM" -> Pair(i.toDouble(), Units.KILOMETER)
-        i.toInt() > 1 && unit == "KILOMETERS" -> Pair(i.toDouble(), Units.KILOMETER)
-        i.toInt() == 1 && unit == "KILOMETER" -> Pair(i.toDouble(), Units.KILOMETER)
-        else -> null
-    }
-
+    val from = Units.get(fromString) // can be null
+    val to = Units.get(toString) // can be null
+    if (from == null || to == null || (from.unitType != to.unitType)) throw ConversionException(
+        "Conversion from ${from?.plural ?: "???"} to ${to?.plural ?: "???"} is impossible"
+    )
+    return ConversionRequest(i, from, to)
 }
 
 
 
 
-enum class Units(val SIFactor: Double) {
-    KILOMETER(1000.0),
-    M(1.0),
-    CM(0.01);
+enum class Units(val factor: Double, val symbol: String, val plural: String, val unitType: UnitType) {
+    METER(1.0, "m", "meters", UnitType.LENGTH),
+    KILOMETER(1000.0, "km", "kilometers", UnitType.LENGTH),
+    CENTIMETER(0.01, "cm", "centimeters", UnitType.LENGTH),
+    MILLIMETER(0.001, "mm", "millimeters", UnitType.LENGTH),
+    MILE(1609.35, "mi", "miles", UnitType.LENGTH),
+    YARD(0.9144, "yd", "yards", UnitType.LENGTH),
+    FOOT(0.3048, "ft", "feet", UnitType.LENGTH),
+    INCH(0.0254, "in", "inches", UnitType.LENGTH),
+
+    GRAM(0.001, "g", "grams", UnitType.WEIGHT),
+    KILOGRAM(1.0, "kg", "kilograms", UnitType.WEIGHT),
+    MILLIGRAM(0.000001, "mg", "milligrams", UnitType.WEIGHT),
+    POUND(0.453592, "lb", "pounds", UnitType.WEIGHT),
+    OUNCE(0.0283495, "oz", "ounces", UnitType.WEIGHT);
+
+    fun getPluralisedName(i: Double): String = if (i == 1.0) single else plural
+
+    val single: String
+        get() = name.lowercase()
+
+
+    companion object {
+        fun getBy(unitString: String, selector: (Units) -> String): Units? {
+            return Units.entries.firstOrNull { selector(it) == unitString }
+        }
+
+        fun get(string: String): Units? {
+            return when {
+                string.length <= 2 -> getBy(string) { it.symbol }
+                else -> getBy(string) { it.single } ?: getBy(string) { it.plural }
+            }
+        }
+    }
 }
 
+open class ConversionException : IllegalArgumentException {
+    constructor(message: String?) : super(message)
+}
+
+enum class UnitType {
+    WEIGHT, LENGTH;
+}
+
+data class ConversionRequest(val i: Double, val from: Units, val to: Units) {
+    val fromName: String
+        get() = from.getPluralisedName(i)
+}
